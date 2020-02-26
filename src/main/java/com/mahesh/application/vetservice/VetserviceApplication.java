af537571a4dadb0ff12f4bd4d6d046e788ef59e5
chichumahesh <maheramachandran@gmail.com>
2020-02-26 05:15:18 -0500

    adding vetservice

---
+++ src/main/java/com/mahesh/application/vetservice/VetserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mahesh.application.vetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahesh/application/vetservice/VetserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahesh.application.vetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
