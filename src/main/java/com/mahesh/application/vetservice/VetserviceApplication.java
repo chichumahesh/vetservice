fa8db3638fed459068dfb0278dffb7dc66b7a8ff
chichumahesh <maheramachandran@gmail.com>
2020-07-08 06:21:45 -0400

    file changes

--- src/main/java/com/mahesh/application/vetservice/VetserviceApplication.java
+++ src/main/java/com/mahesh/application/vetservice/VetserviceApplication.java
@@ -1,13 +1,47 @@
 package com.mahesh.application.vetservice;
 
+import com.mahesh.application.vetservice.model.MovieModel;
+import com.mahesh.application.vetservice.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class VetserviceApplication {
+
+   // @Autowired
+   // MovieService movieService;
 
     public static void main(String[] args) {
         SpringApplication.run(VetserviceApplication.class, args);
     }
+
+
+    @Bean(name = "movieExecutor")
+    public Executor getCurrentExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(3);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("Async-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
 
 }
--- src/main/java/com/mahesh/application/vetservice/resource/VetServiceResource.java
+++ src/main/java/com/mahesh/application/vetservice/resource/VetServiceResource.java
@@ -1,23 +1,23 @@
 package com.mahesh.application.vetservice.resource;
 
+import com.mahesh.application.vetservice.model.Vet;
+import com.mahesh.application.vetservice.service.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VetServiceResource {
 
-    @Value("${my.grade}")
-    private String grade;
-
-    @Value("${my.name}")
-    private String name;
-
-    @Value("${my.abc}")
-    private String abc;
+    @Autowired
+    VetService vetService;
 
     @GetMapping("/vets")
-    public String getVetService() {
-         return grade + "..."+ name + " "+abc;
+    public ResponseEntity<Vet> getVetService() {
+        Vet vet = vetService.getVets();
+        return new ResponseEntity(vet, HttpStatus.OK);
     }
 }
